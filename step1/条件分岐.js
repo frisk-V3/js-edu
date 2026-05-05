2071030cd67d55c49781bce48813c1afe32f36ae
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-05-05 20:12:07 +0900

    Create 条件分岐.js

---
+++ step1/条件分岐.js
@@ -0,0 +1,12 @@
+const name = "キラ"; //偽名
+const name2 = "夜神月" //本名
+for (let i = 0; i < 20; i++) {
+    //もしiが9だったら
+    if (i === 9) {
+        console.log(name2 + "はデスノートを持っている"); //本名を表示
+    } else {
+        console.log(name + "はデスノートを持っている"); //偽名を表示
+    }
+}
+
+
